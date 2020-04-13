5ae753e985c1192aa7f681d5e11f46c8f38a08a6
shs442000 <shs442000@gmail.com>
2020-04-13 04:03:54 +0200

    initial

---
+++ test.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from nba_api.stats.static import teams
+import numpy as np
+from nba_api.stats.endpoints import leaguegamefinder
+
+
+nba_teams_df = pd.DataFrame(teams.get_teams())
+teams_abbreviation_list = nba_teams_df['abbreviation'].tolist()
+
+
+def get_team_id(x):
+    team_df = nba_teams_df[nba_teams_df['abbreviation'] == x]
+    team_id = team_df.iloc[0][0]
+    return team_id
+
+number_of_games = 100
+compated_team_abb = "LAL"
+for abb in teams_abbreviation_list:
+    if(abb == compated_team_abb):
+        continue
+    first_team_id = get_team_id(compated_team_abb)
+    second_team_id = get_team_id(abb)
+
+    first_team_games_df = leaguegamefinder.LeagueGameFinder(
+        team_id_nullable=first_team_id).get_data_frames()[0].head(number_of_games)
+    second_team_games_df = leaguegamefinder.LeagueGameFinder(
+        team_id_nullable=second_team_id).get_data_frames()[0].head(number_of_games)
+    fig, ax = plt.subplots()
+    first_team_games_df.plot(x='GAME_DATE', y='PTS', ax=ax)
+    second_team_games_df.plot(x='GAME_DATE', y='PTS', ax=ax)
+    ax.legend([compated_team_abb, abb])
+    imgName = (compated_team_abb+'vs'+abb+'.png')
+    plt.savefig(imgName)
+    plt.close(fig)
+    print('done')
+
